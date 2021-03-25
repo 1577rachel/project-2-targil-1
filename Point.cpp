479cf05fbaa78b51f13b8dc9de257f820bf8c366
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-03-25 22:45:42 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,7 +3,6 @@
 void Point::setx(int set_x)
 {
     x = set_x;
-
 }
 
 void Point::sety(int set_y)
@@ -11,23 +10,21 @@ void Point::sety(int set_y)
     y = set_y;
 }
 
-float Point::getx()
+float Point::getx() const
 {
     return x;
 }
 
-float Point::gety()
+float Point::gety() const
 {
     return y;
 }
 
-float Point::distance(Point p1)  //the function gets 2 points and callculatets the distance between them 
+float Point::distance(const Point& p1) const  //the function gets 2 points and callculatets the distance between them 
 {
-    int x_d = pow((getx() - p2.getx()),2);  //(x1-x2)(x1-x2)
-    int y_d = pow((gety() - p2.gety()), 2); //(y1-y2)(y1-y2)
+    int x_d = pow((getx() - p1.getx()),2);  //(x1-x2)(x1-x2)
+    int y_d = pow((gety() - p1.gety()), 2); //(y1-y2)(y1-y2)
     float distance = sqrt(x_d + y_d);   //final calculation for the distance
-
-
     return distance;
 }
 
