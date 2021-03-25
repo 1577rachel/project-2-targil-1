8ab91cc2b90f4e468b6e955680d5fdb7f77a0bfa
eliav-code <51802186+eliav-code@users.noreply.github.com>
2021-03-25 22:44:27 +0200

    Update Polygon.h

--- Polygon.h
+++ Polygon.h
@@ -5,12 +5,10 @@ class Polygon
 	Point* array;  
 	int num;  //number of Vertex`s
 public:
-	
-	Polygon(int numbers);  //constructor
+	Polygon(int number);  //constructor
 	//Polygon();		  //defult constructor
 	Polygon(const Polygon& pp); //copy coonstuctor
 	~Polygon(); //dtor
 	void addPoint(int index,int x,int y);  //function gets point and index and adds point to the array
-	float circumference(); //calculates the circumference of the polygon
+	float circumference() const; //calculates the circumference of the polygon
 };
-
